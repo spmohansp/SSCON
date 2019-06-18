05ef221904483b6643cfbbb0ef3bff5574bb4ac1
ShanmugamNandhini <nandhinishanmugam14.5@gmail.com>
2019-06-18 16:22:12 +0530

    Changes in vision & mission

--- resources/views/main/about/vision-mission.blade.php
+++ resources/views/main/about/vision-mission.blade.php
@@ -27,83 +27,69 @@
 
 
 @section('content')
-    <section class="section-grid-demonstration">
+   <section class="section-grid-demonstration">
         <div class="shell">
             <h2 class="text-bold">Vision & Mission</h2>
             <hr class="divider bg-madison">
-            <div class="row offset-top-60 text-sm-left">
-                <div class="col-xs-12">
+            <div class="cell-sm-8 cell-sm-push-1 offset-top-50 offset-sm-top-0 text-sm-left">
+                <div class="row offset-top-60 text-sm-left">
                     <div class="bg-lightest section-30 inset-left-30 inset-right-30">
-                        <div>
+                        <div class="offset-top-30 offset-sm-top-60">
                             <h6 class="text-bold">Our Vision</h6>
-                            <p> The Shanmugha College of nursing for women is committed to enable the students to achieve and maintain highest standards of the profession in nursing education, practice and research, which is required to prepare a professional nurse to effectively compete at various levels of health care system in the ever changing world.</p>
+                            <p align="justify">&nbsp;&nbsp;&nbsp;The Shanmugha College of nursing for women is committed to enable the students to achieve and maintain highest standards of the profession in nursing education, practice and research, which is required to prepare a professional nurse to effectively compete at various levels of health care system in the ever changing world.</p><br>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="row offset-top-60 text-sm-left">
-                <div class="col-xs-12">
+            <div class="cell-sm-8 cell-sm-push-1 offset-top-50 offset-sm-top-0 text-sm-left">
+                <div class="row offset-top-60 text-sm-left">
                     <div class="bg-lightest section-30 inset-left-30 inset-right-30">
-                        <div>
+                        <div class="offset-top-30 offset-sm-top-60">
                             <h6 class="text-bold">Our Mission</h6>
                             <div class="quote-body">
                                 <center><q>LEARN CARE INSPIRE</q></center>
                             </div>
-                            <p> The mission of The Shanmugha college of nursing for women is to establish as a “Center of Excellence” for nursing education through quality approach, professional development and research.</p><br>
-                            <p><i class="fa fa-hand-o-right" aria-hidden="true" style="margin-left: 30px"></i>&ensp;&ensp;&ensp;To learn from the learned</p>
-                            <p><i class="fa fa-hand-o-right" aria-hidden="true" style="margin-left: 30px"></i>&ensp;&ensp;&ensp;To enlighten people’s knowledge</p>
-                            <p><i class="fa fa-hand-o-right" aria-hidden="true" style="margin-left: 30px"></i>&ensp;&ensp;&ensp;To grow in the field of Nursing</p>
-                            <p><i class="fa fa-hand-o-right" aria-hidden="true" style="margin-left: 30px"></i>&ensp;&ensp;&ensp;To serve the community</p>
+                            <p align="justify"> The mission of The Shanmugha college of nursing for women is to establish as a “Center of Excellence” for nursing education through quality approach, professional development and research.</p><br>
+                            <p align="justify"><i class="fa fa-hand-o-right" aria-hidden="true" style="margin-left: 30px"></i>&ensp;&ensp;&ensp;To learn from the learned</p>
+                            <p align="justify"><i class="fa fa-hand-o-right" aria-hidden="true" style="margin-left: 30px"></i>&ensp;&ensp;&ensp;To enlighten people’s knowledge</p>
+                            <p align="justify"><i class="fa fa-hand-o-right" aria-hidden="true" style="margin-left: 30px"></i>&ensp;&ensp;&ensp;To grow in the field of Nursing</p>
+                            <p align="justify"><i class="fa fa-hand-o-right" aria-hidden="true" style="margin-left: 30px"></i>&ensp;&ensp;&ensp;To serve the community</p>
                         </div>
                     </div>
                 </div>
             </div>
-           <!--  <div class="row offset-top-60 text-sm-left">
-                <div class="col-xs-12">
+           
+            <div class="cell-sm-8 cell-sm-push-1 offset-top-50 offset-sm-top-0 text-sm-left">
+                <div class="row offset-top-60 text-sm-left">
                     <div class="bg-lightest section-30 inset-left-30 inset-right-30">
-                        <div>
-                            <h6 class="text-bold">Objective</h6>
-                            <p><i class="fa fa-hand-o-right" aria-hidden="true"></i>&ensp;&ensp;&ensp;Nurture of Quality in Pharmaceutical Education</p>
-                            <p><i class="fa fa-hand-o-right" aria-hidden="true"></i>&ensp;&ensp;&ensp;Creating Innovative Pharma teaching practices</p>
-                            <p><i class="fa fa-hand-o-right" aria-hidden="true"></i>&ensp;&ensp;&ensp;Providing Continuous Education Programme for practicing pharmacists</p>
-                            <p><i class="fa fa-hand-o-right" aria-hidden="true"></i>&ensp;&ensp;&ensp;Promotes Research & Development</p>
-                            <p><i class="fa fa-hand-o-right" aria-hidden="true"></i>&ensp;&ensp;&ensp;Health Care Service for the welfare of Community</p>
-                            <p><i class="fa fa-hand-o-right" aria-hidden="true"></i>&ensp;&ensp;&ensp;Service to the industry by producing quality pharmacists</p>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
-            <div class="row offset-top-60 text-sm-left">
-                <div class="col-xs-12">
-                    <div class="bg-lightest section-30 inset-left-30 inset-right-30">
-                        <div>
+                        <div class="offset-top-30 offset-sm-top-60">
                             <h6 class="text-bold">Philosophy</h6>
-                            <p> We, the faculty of this college, believe that this B.Sc (N) 4 years programme will be able to prepare our students to meet the standards of the profession, follow the code of ethics & deliver the quality and cost effective care in any settings.</p>
+                            <p align="justify"> We, the faculty of this college, believe that this B.Sc (N) 4 years programme will be able to prepare our students to meet the standards of the profession, follow the code of ethics & deliver the quality and cost effective care in any settings.</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="row offset-top-60 text-sm-left">
-                <div class="col-xs-12">
+            <div class="cell-sm-8 cell-sm-push-1 offset-top-50 offset-sm-top-0 text-sm-left">
+                <div class="row offset-top-60 text-sm-left">
                     <div class="bg-lightest section-30 inset-left-30 inset-right-30">
-                        <div>
+                        <div class="offset-top-30 offset-sm-top-60">
                             <h6 class="text-bold">Aims</h6>
-                           <p><i class="fa fa-hand-o-right" aria-hidden="true" style="margin-left: 30px"></i>&ensp;&ensp;&ensp;Prepare graduates to assume responsibilities as professional, competent nurses and Midwives in providing promotive, preventive, curative and rehabilitative services.</p>
-                        <p><i class="fa fa-hand-o-right" aria-hidden="true" style="margin-left: 30px"></i>&ensp;&ensp;&ensp;Prepare nurses who can make independent decisions in nursing situations, protect the rights of and facilitate individuals and groups in pursuit of health, function in the hospital, community nursing services, and conduct research studies in the areas of nursing practice. They are also expected to assume the role of teacher, supervisor and manager in a clinical and public health setting.</p><br>
+                            <p align="justify"><i class="fa fa-hand-o-right" aria-hidden="true" style="margin-left: 30px"></i>&ensp;&ensp;&ensp;Prepare graduates to assume responsibilities as professional, competent nurses and Midwives in providing promotive, preventive, curative and rehabilitative services.</p>
+                            <p align="justify"><i class="fa fa-hand-o-right" aria-hidden="true" style="margin-left: 30px"></i>&ensp;&ensp;&ensp;Prepare nurses who can make independent decisions in nursing situations, protect the rights of and facilitate individuals and groups in pursuit of health, function in the hospital, community nursing services, and conduct research studies in the areas of nursing practice. They are also expected to assume the role of teacher, supervisor and manager in a clinical and public health setting.</p><br>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="row offset-top-60 text-sm-left">
-                <div class="col-xs-12">
+            <div class="cell-sm-8 cell-sm-push-1 offset-top-50 offset-sm-top-0 text-sm-left">
+                <div class="row offset-top-60 text-sm-left">
                     <div class="bg-lightest section-30 inset-left-30 inset-right-30">
-                        <div>
+                        <div class="offset-top-30 offset-sm-top-60">
                             <h6 class="text-bold">Objectives</h6>
                             <p align="justify">&nbsp;&nbsp;&nbsp;Apply knowledge from physical, biological and behavioral sciences, medicine including alternative systems and nursing in providing nursing care to individuals, families and communities.</p><br>  
-                            <p><i class="fa fa-hand-o-right" aria-hidden="true" style="margin-left: 30px"></i>&ensp;&ensp;&ensp;Demonstrate understanding of life style and other factors, which affect health of individuals and groups.</p>
-                            <p><i class="fa fa-hand-o-right" aria-hidden="true" style="margin-left: 30px"></i>&ensp;&ensp;&ensp;Provide nursing care based on steps of nursing process in collaboration with the individuals and groups.</p>
-                            <p><i class="fa fa-hand-o-right" aria-hidden="true" style="margin-left: 30px"></i>&ensp;&ensp;&ensp;Demonstrate critical thinking skill in making decisions in all situations in order to provide quality care.</p>
-                            <p><i class="fa fa-hand-o-right" aria-hidden="true" style="margin-left: 30px"></i>&ensp;&ensp;&ensp;Utilize the latest trends and technology in providing health care.</p>
+                            <p align="justify"><i class="fa fa-hand-o-right" aria-hidden="true" style="margin-left: 30px"></i>&ensp;&ensp;&ensp;Demonstrate understanding of life style and other factors, which affect health of individuals and groups.</p>
+                            <p align="justify"><i class="fa fa-hand-o-right" aria-hidden="true" style="margin-left: 30px"></i>&ensp;&ensp;&ensp;Provide nursing care based on steps of nursing process in collaboration with the individuals and groups.</p>
+                            <p align="justify"><i class="fa fa-hand-o-right" aria-hidden="true" style="margin-left: 30px"></i>&ensp;&ensp;&ensp;Demonstrate critical thinking skill in making decisions in all situations in order to provide quality care.</p>
+                            <p align="justify"><i class="fa fa-hand-o-right" aria-hidden="true" style="margin-left: 30px"></i>&ensp;&ensp;&ensp;Utilize the latest trends and technology in providing health care.</p>
                         </div>
                     </div>
                 </div>
@@ -111,6 +97,4 @@
 
         </div>
     </section>
-
-
 @endsection
